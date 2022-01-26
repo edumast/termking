88380ae77418a0f70df30aa50ff8ddf747ccebd3
edumast <euedu90@gmail.com>
2022-01-26 18:24:51 -0300

    fix broken

--- style/wlc.py
+++ style/wlc.py
@@ -1,10 +1,6 @@
 import os,sys,time,random
 print("")
-<<<<<<< HEAD
 name='dev_mast' #end
-=======
-name='usr' #end
->>>>>>> 3829972 (update)
 os.system("tput setaf 1")
 color=["\033[1;31m","\033[1;32m"]
 m = ("welcome to termking_os %s \n" %(name)) 
@@ -12,4 +8,4 @@ for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
     time.sleep(0.06)
-print("") 
+print("")
--- term.sh
+++ term.sh
@@ -2,18 +2,14 @@
 data_file="databased"
 local_directory=`echo $PWD | rev | cut -d'/' -f 1 | rev`
 source ~/$local_directory"/"$data_file/database
-version="1.0" 
+version="1.0"
 init_system=true
 editor_init=false
 cont_app=1
 app_number="m"
 python3 style/wlc.py
 tput setaf 2
-<<<<<<< HEAD
-echo "##########################"
-=======
 echo "##########################" |lolcat
->>>>>>> 3829972 (update)
 while [ $init_system == true ];do
   #<source>
   source ~/$local_directory"/"$data_file/database
@@ -23,11 +19,7 @@ while [ $init_system == true ];do
  timestamp=$(date +"%T")
 tput setaf 197
 tput bold
-<<<<<<< HEAD
  echo "$name_usr |" $timestamp "|termking"
-=======
- echo "$name_usr |" $timestamp "|termking" 
->>>>>>> 3829972 (update)
  tput setaf 2
  read command
  #<command>
@@ -37,14 +29,14 @@ tput bold
   command_pt2=`echo $command | cut -d' ' -f2`
   command_pt3=`echo $command | cut -d' ' -f3`
   #<distribution_parts>
-
+  
   #<reboot>
   if [ "$command" == "reboot" ];
   then
     ./config_system/reboot.sh
   fi
   #<reboot>
-
+  
   #<shutdown>
   if [ "$command" == "shutdown" ];
   then
@@ -62,38 +54,31 @@ tput bold
     fi
 fi
   #<mep>
-
+  
   #<update>
   if [ "$command" == "update" ];then
     cd update
     ./update_software
   fi
   #<update>
-
+  
   #<format>
 if [ "$command" == "format" ];then
 command=" "
   ./debug/deb_rm_in
 fi
   #<format>
-
-  
   if [ "$command_pt1" == "clin" ];then
 $command_pt2 $command_pt3
   fi
 
 #<commands>
   if [ "$command" == "comman" ];then
-<<<<<<< HEAD
-echo "reboot | shutdown | update | rename_usr | see_info | change info
-=======
-echo "reboot | shutdown | update | rename_usr | info | change info
->>>>>>> 3829972 (update)
-down | app | mep | format |comman "
+echo "reboot | shutdown | update | rename_usr | info | change info"
   fi
 #<commands>
 
-  #<see information>
+#<see information>
   if [ "$command_pt1" == "info" ];then
     if [ "$command_pt2" == "age" ];then
       echo $age_usr
@@ -101,64 +86,62 @@ down | app | mep | format |comman "
   fi
   #<see information>
 
-  #<change_info>
-  if [ "$command_pt1" == "change" ];then
-    #<name>
-      if [ "$command_pt2" == "name" ];then
-        sed -i "s/name_usr=.*#end/name_usr=$command_pt3 #end/g" $data_file/database
+#<change_info>
+   
+ if [ $command_pt1 == "change" ];then
+   
+   #<name> 
+    if [ "$command_pt2" == "name" ];then
+              sed -i "s/name_usr=.*#end/name_usr=$command_pt3 #end/g" $data_file/database
         sed -i "s/name=.*#end/name='$command_pt3' #end/g" style/wlc.py
       fi
-     #<name>
-
+         #<name>
     if [ "$command_pt2" == "age" ];then
       sed -i "s/age_usr=.*#end/age_usr=$command_pt3 #end/g" $data_file/database
     fi
-  fi
+    fi
+
   #<change_info>
 
-  #<lok>
+#<lok>
   if [ "$command_pt1" == "lok" ];then
-    if [ "$command_pt2" == "install" ];then
-<<<<<<< HEAD
+      if [ "$command_pt2" == "install" ];then
       chmod 755 ~/$local_directory/softwares/install_app/*
       ~/$local_directory/softwares/install_app/install_$command_pt3
-=======
       ~/$local_directory/softwares/adl/$command_pt3
->>>>>>> 3829972 (update)
-    fi
+   fi
   fi
   #<lok>
 
-  #<down>
+#<down>
  if [ "$command_pt1" == "down" ];then
-  
    if [ "$command_pt2" == "install" ];then
   git clone $command_pt3
   directory_down=`echo $command_pt3 | rev | cut -d'/' -f 1 | rev`
-  rm -rf $directory_down/README.md 
+  rm -rf $directory_down/README.md
 mv $directory_down ~/$local_directory/softwares/software_app
-    fi
+   fi
 
-       #<remove>
+#<remove>
     if [ "$command_pt2" == "remove" ];then
     rm -rf softwares/software_app/$command_pt3
     fi
-       #<remove>
 
+#<remove>
  fi
    #<down>
-
-  #<enter_app>
+ 
+#<enter_app>
   if [ "$command_pt1" == "apps" ];then
     if [ "$command_pt2" == "enter" ];then
-    enter_app=$command_pt3  
+    enter_app=$command_pt3
      ~/$local_directory/softwares/software_app/$enter_app/init.sh
     else
     echo app?
     echo ~×~
     ls ~/$local_directory/softwares/software_app
-    tput setaf 2
-    tput bold
+      tput setaf 2
+      tput bold
     echo ~×~
     read enter_app
     if [ "$enter_app" != "" ];then
@@ -171,8 +154,8 @@ mv $directory_down ~/$local_directory/softwares/software_app
     fi
   #<enter_app>
   fi
-
   #<err1>
+
   if [ "$err1" == true ];then
     tput bold
  tput setaf 1
@@ -181,6 +164,4 @@ mv $directory_down ~/$local_directory/softwares/software_app
  err1=false
 fi
   #<err1>
-
 done
-
