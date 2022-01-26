ef4fed031e51b99f12df9253309cb80b3da86cf6
edumast <euedu90@gmail.com>
2022-01-26 20:06:21 -0300

    update

---
+++ style/wlc.py
@@ -0,0 +1,11 @@
+import os,sys,time,random
+print("")
+name='dev_mast' #end
+os.system("tput setaf 1")
+color=["\033[1;31m","\033[1;32m"]
+m = ("welcome to termking_os %s \n" %(name)) 
+for msg in m:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    time.sleep(0.06)
+print("")
