f34c7c66f2166a752051a164fd00539a90bbb740
edumast <euedu90@gmail.com>
2022-01-27 17:15:57 -0300

    up

---
+++ softwares/editor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+enter_in_app="editor"
+cod_definity_editor=""
+select_file_option=input("1.create  2.enter")
+select_file=input("which file?")
+if (select_file_option=="1"):
+    os.system("touch data/editor_py/%s" %(select_file))
+file=("data/editor_py/")+(select_file)
+while(enter_in_app=="editor"):
+    cod_definity_editor=cod_definity_editor
+    cod_editor=input("")
+    cod_editor=cod_editor
+    if (cod_editor=="save"):
+        file=open(str(file), "a")
+        file.write(cod_definity_editor)
+        file=open(str(file), "a")
+        print("finaly")
+        cod_editor=""
+    if (cod_definity_editor==""):
+        cod_definity_editor=(cod_editor)
+    else:
+        cod_definity_editor=(cod_definity_editor)+"\n"+(cod_editor)
